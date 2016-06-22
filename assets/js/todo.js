18ddfd379b14aec2bf01eeff69a9a0cb8b5d4371
netecodes <dotneko@gmail.com>
2016-06-22 16:01:22 +0200

    add Style and Add box

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,14 +1,23 @@
 // todo.js
 
-// Set task as done by clicking
-$('li').click(function(){
+// Set task as Done by clicking
+$('ul').on('click', 'li', function(){
 	$(this).toggleClass('done');
 	});
 
 // Click on X to delete Todo
-$('span').click(function(event){
+$('ul').on('click', 'span', function(event){
 	$(this).parent().fadeOut(500,function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
+})
+
+// Add new Todo
+$("input[type='text']").keypress(function(event){
+	if (event.which === 13) {
+		var todoText = $(this).val();
+		$(this).val("");
+		$('ul').append('<li><span>X</span> ' + todoText +'</li>');
+	}
 })
