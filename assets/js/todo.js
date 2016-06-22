275b494215b84eb0f2f3fd46253322ec7d5f14b9
netecodes <dotneko@gmail.com>
2016-06-22 14:29:47 +0200

    add Done and Remote functionality

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,1 +1,14 @@
-//todo.js
+// todo.js
+
+// Set task as done by clicking
+$('li').click(function(){
+	$(this).toggleClass('done');
+	});
+
+// Click on X to delete Todo
+$('span').click(function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+})
